d262a7c9c4af3ba457308bf19f25c4e4cb7d5b42
tom-code <petrilak.tomas@gmail.com>
2017-10-07 15:09:37 +0200

    display return code and properly handle exec

--- pt.cc
+++ pt.cc
@@ -17,36 +17,48 @@ void demo_function() {
   int f = open("pt.cc", O_RDONLY);
   lseek(f, 10, SEEK_SET);
   close(f);
-  getpid();
   execl("/bin/ls", "/bin/ls", nullptr);
 }
 
 int main()
 {
-  pid_t child;
-
   auto syscall_names = get_syscall_name_vector();
 
-  child = fork();
+  pid_t child = fork();
   if (child == 0) {  //forked child
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     kill(getpid(), SIGINT);
     demo_function();
   } else {  //parent
-      int err;
-      struct user_regs_struct uregs;
+      wait(NULL);   // wait for initial signal
 
+      ptrace(PTRACE_SYSCALL, child, 0, 0);
       wait(NULL);
+
       while (1) {
-        err = ptrace(PTRACE_SYSCALL, child, 0, 0); // wait for syscall entry
+        int err = ptrace(PTRACE_SYSCALL, child, 0, 0); // wait for syscall entry
         if (err < 0) break;
         wait(NULL);
 
+        struct user_regs_struct uregs;
         err = ptrace(PTRACE_GETREGS, child, 0, &uregs);
-        printf("detected syscall name=%s first_arg=%llx\n", syscall_names[uregs.orig_rax].c_str(), uregs.rdi);
+        if (err < 0) break;
+        if (uregs.orig_rax == 59) { // special handling for exec
+          printf(">>syscall exec handled\n");
+          err = ptrace(PTRACE_SYSCALL, child, 0, 0);
+          wait(NULL);
+          err = ptrace(PTRACE_SYSCALL, child, 0, 0);
+          wait(NULL);
+          continue;
+        }
+        printf(">>syscall name=%s    first_arg=0x%llx", syscall_names[uregs.orig_rax].c_str(), uregs.rdi);
 
         err = ptrace(PTRACE_SYSCALL, child, 0, 0); // wait for syscall return
-        wait(NULL);        
+        if (err < 0) break;
+
+        err = ptrace(PTRACE_GETREGS, child, 0, &uregs);
+        printf("  return=0x%llx\n", uregs.rax);
+        wait(NULL);
       }
   }
 }
